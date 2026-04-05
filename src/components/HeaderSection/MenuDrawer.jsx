dd271c50238da440a79c10fbe3b3376b93f2d94b
phwnganh <phuonganh25102003@gmail.com>
2026-04-05 18:38:54 +0700

    feat: insert menu drawer when user click the menu hamburger in tablet and mobile uis

---
+++ src/components/HeaderSection/MenuDrawer.jsx
@@ -0,0 +1,22 @@
+import MenuCloseIcon from '../../assets/icon-menu-close.svg'
+const MenuDrawer = ({onCloseMenuDrawer}) => {
+    return (
+        <nav role={"menu"} className={"px-9 py-6.75 bg-white ml-auto z-10 w-2/3 max-w-xs h-full transition-transform duration-300 translate-x-0"}>
+            <div className={"flex flex-col gap-22"}>
+                <button type={"button"} onClick={onCloseMenuDrawer} className={"flex justify-end shrink-0"}>
+                    <img src={MenuCloseIcon} alt={"menu-close-icon"} />
+                </button>
+                <ul role={"list"} className={"flex flex-col gap-4"}>
+                    <li className={"text-preset-5 leading-preset-5 text-navy-950"}>Home</li>
+                    <li className={"text-preset-5 leading-preset-5 text-navy-950"}>New</li>
+                    <li className={"text-preset-5 leading-preset-5 text-navy-950"}>Popular</li>
+                    <li className={"text-preset-5 leading-preset-5 text-navy-950"}>Trending</li>
+                    <li className={"text-preset-5 leading-preset-5 text-navy-950"}>Categories</li>
+                </ul>
+            </div>
+
+        </nav>
+    );
+};
+
+export default MenuDrawer;
--- src/components/HeaderSection/index.jsx
+++ src/components/HeaderSection/index.jsx
@@ -1,16 +1,31 @@
 import logo from '../../assets/logo.svg'
 import MenuSection from "./MenuSection.jsx";
 import HamburgerIcon from '../../assets/icon-menu.svg'
+import {useState} from "react";
+import MenuDrawer from "./MenuDrawer.jsx";
 const HeaderSection = () => {
+    const [openMenuDrawer, setOpenMenuDrawer] = useState(false)
+    const handleOpenMenuDrawer = () => {
+        setOpenMenuDrawer(true)
+    }
+    const handleCloseMenuDrawer = () => {
+        setOpenMenuDrawer(false)
+    }
     return (
         <header className={"flex items-center justify-between"}>
             <div className={"flex justify-center items-center shrink-0"}>
                 <img src={logo} alt="logo" />
             </div>
             <MenuSection/>
-            <div className={"flex items-center justify-center shrink-0 md:hidden"}>
+            <button type={"button"} onClick={handleOpenMenuDrawer} className={"flex items-center justify-center shrink-0 md:hidden"}>
                 <img src={HamburgerIcon} alt={"hamburger-icon"}/>
-            </div>
+            </button>
+                {openMenuDrawer &&
+                <div aria-live={"polite"} className={"fixed inset-0 z-50 flex justify-end"}>
+                    <div className={"absolute inset-0 bg-[#979797] opacity-50 transition-opacity duration-300"}></div>
+                    <MenuDrawer onCloseMenuDrawer={handleCloseMenuDrawer}/>
+                </div>}
+
         </header>
     );
 };
