c038882e7a6af7f81a1d4bb82766ba4485d56bd4
phwnganh <phuonganh25102003@gmail.com>
2026-04-05 18:53:27 +0700

    fix: the path displaying imgs 3

--- src/components/FooterSection/index.jsx
+++ src/components/FooterSection/index.jsx
@@ -3,17 +3,17 @@ import LatestCard from "./LatestCard.jsx";
 const FooterSection = () => {
     const LATESTCARDITEM = [
         {
-           img: '/public/image-retro-pcs.jpg',
+           img: '/image-retro-pcs.jpg',
             title: 'Reviving Retro PCs',
             description: 'What happens when old PCs are given modern upgrades?'
         },
         {
-            img: '/public/image-top-laptops.jpg',
+            img: '/image-top-laptops.jpg',
             title: 'Top 10 Laptops of 2022',
             description: 'Our best picks for various needs and budgets.'
         },
         {
-            img: '/public/image-gaming-growth.jpg',
+            img: '/image-gaming-growth.jpg',
             title: 'The Growth of Gaming',
             description: 'How the pandemic has sparked fresh opportunities.'
         }
