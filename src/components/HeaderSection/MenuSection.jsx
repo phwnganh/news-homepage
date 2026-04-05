c2fe36b69f195f92742833b214593b9ffba06e96
phwnganh <phuonganh25102003@gmail.com>
2026-04-05 19:00:55 +0700

    fix: web accessibility

--- src/components/HeaderSection/MenuSection.jsx
+++ src/components/HeaderSection/MenuSection.jsx
@@ -1,14 +1,12 @@
 
 const MenuSection = () => {
     return (
-        <nav role={"menu"}>
-            <ul role={"list"} className={"md:flex hidden items-center gap-10"}>
-                <li className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>Home</li>
-                <li className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>New</li>
-                <li className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>Popular</li>
-                <li className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>Trending</li>
-                <li className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>Categories</li>
-            </ul>
+        <nav role={"menu"} aria-label="menu" className={"md:flex hidden items-center gap-10"}>
+                <button role={"menuitem"} tabIndex={0} className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>Home</button>
+                <button role={"menuitem"} tabIndex={0} className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>New</button>
+                <button role={"menuitem"} tabIndex={0} className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>Popular</button>
+                <button role={"menuitem"} tabIndex={0} className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>Trending</button>
+                <button role={"menuitem"} tabIndex={0} className={"text-preset-6 leading-preset-6 text-navy-600 hover:text-red-500 cursor-pointer"}>Categories</button>
         </nav>
     );
 };
