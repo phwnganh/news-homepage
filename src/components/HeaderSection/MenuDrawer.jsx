68db16a5208dc2c7c23996cac75be245041cb0ee
phwnganh <phuonganh25102003@gmail.com>
2026-04-05 18:40:00 +0700

    fix: remove unnessary attribute in element

--- src/components/HeaderSection/MenuDrawer.jsx
+++ src/components/HeaderSection/MenuDrawer.jsx
@@ -1,7 +1,7 @@
 import MenuCloseIcon from '../../assets/icon-menu-close.svg'
 const MenuDrawer = ({onCloseMenuDrawer}) => {
     return (
-        <nav role={"menu"} className={"px-9 py-6.75 bg-white ml-auto z-10 w-2/3 max-w-xs h-full transition-transform duration-300 translate-x-0"}>
+        <nav role={"menu"} className={"px-9 py-6.75 bg-white ml-auto w-2/3 max-w-xs h-full transition-transform duration-300 translate-x-0"}>
             <div className={"flex flex-col gap-22"}>
                 <button type={"button"} onClick={onCloseMenuDrawer} className={"flex justify-end shrink-0"}>
                     <img src={MenuCloseIcon} alt={"menu-close-icon"} />
