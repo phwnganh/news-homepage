c38d1e9d0635df588973310b78d27c220f064954
phwnganh <phuonganh25102003@gmail.com>
2026-04-05 21:42:37 +0700

    fix: web accessibility 4

--- src/components/MainSection/SidebarSection/SidebarArticleCard.jsx
+++ src/components/MainSection/SidebarSection/SidebarArticleCard.jsx
@@ -3,8 +3,8 @@ const SidebarArticleCard = ({title, aria_title, aria_description, description, i
     return (
         <div className={"flex flex-col gap-8"}>
             <div className={"flex flex-col gap-2"} aria-labelledby={aria_title} aria-describedby={aria_description}>
-                <h2 id={aria_title} className={"text-white text-preset-4 leading-preset-4 font-preset-4 cursor-pointer hover:text-gold-400"}>{title}</h2>
-                <h3 id={aria_description} className={"text-gray-300 text-preset-6 leading-preset-6"}>{description}</h3>
+                <button type={"button"} aria-expanded={"false"} id={aria_title} className={"text-white text-preset-4 leading-preset-4 font-preset-4 cursor-pointer hover:text-gold-400"}>{title}</button>
+                <p id={aria_description} className={"text-gray-300 text-preset-6 leading-preset-6"}>{description}</p>
             </div>
             {!isLast && <hr className={"text-navy-600"}/>}
         </div>
